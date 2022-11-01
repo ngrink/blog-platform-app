98bcdac99583811046837fd4f3f2ab8b3306e778
Nikolay Grinko <ngrink.io@gmail.com>
2022-11-01 19:38:47 +0300

    Add fontawesome library

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 import { App } from './App';
+import './utils/libs/fontawesome';
 import './assets/css/index.css';
 
 
---
+++ src/utils/libs/fontawesome/fontawesome.js
@@ -0,0 +1,12 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars, faEye, faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faMessage as faMessageRegular ,  faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons';
+
+
+library.add(
+    faBars,
+    faEye,
+    faMessageRegular,
+    faHeart,
+    faHeartRegular
+)
