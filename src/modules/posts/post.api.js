d6619e9a6d52ec8d0647deceb5967d68786fff9e
Nikolay Grinko <ngrink.io@gmail.com>
2022-11-05 00:03:50 +0300

    Add api layer for "posts" module

---
+++ src/modules/posts/post.api.js
@@ -0,0 +1,44 @@
+import { $axios } from "../../utils/libs/axios";
+
+
+export class PostAPI {
+    static async createPost(postData) {
+        return await $axios.post(`/posts/`, {...postData});
+    }
+
+    static async getAllPosts() {
+        return await $axios.get(`/posts/`)
+    }
+
+    static async getPost(postId) {
+        return await $axios.get(`/posts/${postId}`)
+    }
+
+    static async updatePost(postId, postData) {
+        return await $axios.patch(`/posts/${postId}`, {...postData})
+    }
+
+    static async deletePost(postId) {
+        return await $axios.delete(`/posts/${postId}`)
+    }
+
+    static async publicatePost(postId) {
+        return await $axios.delete(`/posts/${postId}/publish`)
+    }
+
+    static async likePost(postId) {
+        return await $axios.delete(`/posts/${postId}/like`)
+    }
+
+    static async unlikePost(postId) {
+        return await $axios.delete(`/posts/${postId}/unlike`)
+    }
+
+    static async getPostComments(postId) {
+        return await $axios.delete(`/posts/${postId}/comments`)
+    }
+
+    static async createPostComment(postId, commentData) {
+        return await $axios.delete(`/posts/${postId}/comments`, {...commentData})
+    }
+}
