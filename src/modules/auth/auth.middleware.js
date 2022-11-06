fd9c17743962a5e1d1fe4ce125598319edef2a0b
Nikolay Grinko <ngrink.io@gmail.com>
2022-11-06 14:56:32 +0300

    Add optionalAuthorized middleware
    That middleware parse token and save it to "req.token". If token doesn't exists, middleware doesn't throw "unauthorized" exception

--- src/modules/auth/auth.middleware.js
+++ src/modules/auth/auth.middleware.js
@@ -4,19 +4,32 @@ import { AuthTokenService } from './auth.token.service';
 
 export class AuthMiddleware {
     static async authorized(req, res, next) {
+        AuthMiddleware._parseToken(() => {
+            next(AuthError.Unauthorized());
+        });
+    }
+
+    static async optionalAuthorized(req, res, next) {
+        AuthMiddleware._parseToken(() => {
+            req.token = {};
+            next();
+        });
+    }
+
+    static async _parseToken(onError) {
         try {
             const { accessToken } = req.cookies;
             if (!accessToken) {
-                next(AuthError.Unauthorized());
+                onError();
             }
             const validToken = await AuthTokenService.verifyAccessToken(accessToken);
             if (!validToken) {
-                next(AuthError.Unauthorized());
+                onError();
             }
             req.token = validToken;
             next();
         } catch (e) {
-            next(AuthError.Unauthorized())
+            onError();
         }
     }
 }
