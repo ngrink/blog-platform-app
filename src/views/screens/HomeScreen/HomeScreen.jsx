46aaa9ab2801d4b56ccc639e9aef71d968411ce9
Nikolay Grinko <ngrink.io@gmail.com>
2022-11-07 17:17:10 +0300

    Add "HomeScreen" component

---
+++ src/views/screens/HomeScreen/HomeScreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Box } from '@chakra-ui/react';
+
+import { PostCardListContainer } from '../../containers/PostCardListContainer';
+
+
+export const HomeScreen = () => {
+    return (
+        <Box maxW="640px" mx="auto" pt="50px">
+            <PostCardListContainer />
+        </Box>
+    )
+}
