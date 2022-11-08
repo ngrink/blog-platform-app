bcfd81bc0d865ddba277b3625606473ac9784124
Nikolay Grinko <ngrink.io@gmail.com>
2022-11-08 16:45:53 +0300

    Refactoring

--- src/views/components/PostCard/PostCard.jsx
+++ src/views/components/PostCard/PostCard.jsx
@@ -24,65 +24,75 @@ export const PostCard = ({
     onUnlike
 }) => {
     return (
-        <Link to={`/posts/${slug}`}>
-            <Box className={cl.card}>
-                {preview && <Box>
+        <Box className={cl.card}>
+            {preview &&
+                <Box>
                     <AspectRatio ratio={16 / 9}>
                         <Image src={preview} objectFit="cover"/>
                     </AspectRatio>
-                </Box>}
-                <Box className={cl.content}>
-                    <VStack gap="10px" alignItems="flex-start">
+                </Box>
+            }
+            <Box className={cl.cardInner}>
+                <VStack gap="12px" alignItems="flex-start">
+                    <Box className={cl.header}>
                         <UserAvatar
                             src={author.profile.avatar}
                             name={author.profile.fullname}
                             subtitle={moment(createdAt).fromNow()}
-                        />
+                            />
+                    </Box>
+                    <Box className={cl.tags}>
                         <Flex wrap="wrap" gap="10px">
                             {tags.map(tag => (
-                                <Tag key={tag} variant="subtle">
+                                <Tag key={tag} variant="subtle" fontSize={13}>
                                     {tag}
                                 </Tag>
                             ))}
                         </Flex>
-                        <Heading fontSize="26px" noOfLines={3}>{title}</Heading>
-                        <Text>{description}</Text>
-                    </VStack>
-                </Box>
-                <Box className={cl.footer}>
-                    <Flex gap="10px" w="100%" align="center">
-                        <Button onClick={likes.isLikedByUser
-                            ? (e) => {onUnlike(_id)}
-                            : (e) => {onLike(_id)}
-                        }
-                            variant={likes.isLikedByUser ? "solid" : "outline"}
-                            colorScheme={likes.isLikedByUser ? "blue" : "gray"}
-                            leftIcon={likes.isLikedByUser
-                                ? <FontAwesomeIcon icon="fa-solid fa-heart"/>
-                                : <FontAwesomeIcon icon="fa-regular fa-heart"/>
-                        }>
-                            {likes.count}
-                        </Button>
-                        <Button
-                            variant={likes.isLikedByUser ? "solid" : "outline"}
-                            colorScheme={comments.isCommentedByUser ? "blue" : "gray"}
-                            leftIcon={comments.isCommentedByUser
-                                ? <FontAwesomeIcon icon="fa-solid fa-message"/>
-                                : <FontAwesomeIcon icon="fa-regular fa-message"/>
-                            }
-                        >
-                            {comments.count}
-                        </Button>
-                        <Spacer />
-                        <Flex gap="8px" align="center">
-                            <FontAwesomeIcon icon="fa-solid fa-eye"/>
-                            <Text fontWeight={500}>
-                                {views}
-                            </Text>
-                        </Flex>
-                    </Flex>
-                </Box>
+                    </Box>
+                    <Box className={cl.content}>
+                        <Link to={`/posts/${slug}`}>
+                            <VStack gap="12px" alignItems="flex-start">
+                                <Heading fontSize="26px" noOfLines={3}>{title}</Heading>
+                                <Text>{description}</Text>
+                            </VStack>
+                        </Link>
+                    </Box>
+                </VStack>
             </Box>
-        </Link>
+            <Box className={cl.footer}>
+                <Flex gap="10px" w="100%" align="center">
+                    <Button onClick={likes.isLikedByUser
+                        ? (e) => {onUnlike(_id)}
+                        : (e) => {onLike(_id)}
+                    }
+                        variant={likes.isLikedByUser ? "solid" : "outline"}
+                        colorScheme={likes.isLikedByUser ? "blue" : "gray"}
+                        leftIcon={likes.isLikedByUser
+                            ? <FontAwesomeIcon icon="fa-solid fa-heart"/>
+                            : <FontAwesomeIcon icon="fa-regular fa-heart"/>
+                    }>
+                        {likes.count}
+                    </Button>
+                    <Button
+                        variant={comments.isCommentedByUser ? "solid" : "outline"}
+                        colorScheme={comments.isCommentedByUser ? "blue" : "gray"}
+                        leftIcon={comments.isCommentedByUser
+                            ? <FontAwesomeIcon icon="fa-solid fa-message"/>
+                            : <FontAwesomeIcon icon="fa-regular fa-message"/>
+                        }
+                    >
+                        {comments.count}
+                    </Button>
+                    <Spacer />
+                    <Flex gap="8px" align="center">
+                        <FontAwesomeIcon icon="fa-solid fa-eye"/>
+                        <Text fontWeight={500}>
+                            {views}
+                        </Text>
+                    </Flex>
+                </Flex>
+            </Box>
+        </Box>
     )
 }
