dbe849b5308ce3b84da082140c2486fdffd4a657
Nikolay Grinko <ngrink.io@gmail.com>
2022-11-10 21:44:41 +0300

    Add "PostScreen", "PostContainer", "Post" components

---
+++ src/views/components/Post/Post.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { Box, Flex, Heading, Spacer, Text, VStack } from '@chakra-ui/react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm'
+import remarkToc from 'remark-toc';
+
+import { PostHeader } from '../PostHeader';
+import { PostTags } from '../PostTags';
+import { PostPreview } from '../PostPreview';
+import { PostFooter } from '../PostFooter';
+import cl from "./Post.module.scss";
+
+
+export const Post = ({
+    _id: postId,
+    title,
+    description,
+    content,
+    slug,
+    author,
+    createdAt,
+    preview,
+    tags,
+    likes,
+    comments,
+    views,
+    onLike,
+    onUnlike
+}) => {
+    return (
+        <Flex className={cl.card} flexDirection="column">
+            <VStack className={cl.cardContainer} gap="16px" alignItems="flex-start">
+                <PostHeader
+                    author={author}
+                    createdAt={createdAt}
+                />
+                <PostTags
+                    tags={tags}
+                />
+            </VStack>
+            <Box className={cl.cardContainer} pt="16px" pb="32px">
+                <VStack gap="12px" alignItems="flex-start">
+                    <Heading as="h1" size="lg">{title}</Heading>
+                    <Text>{description}</Text>
+                </VStack>
+            </Box>
+            {preview &&
+                <PostPreview
+                    preview={preview}
+                />
+            }
+            <Box className={cl.cardContainer} py="24px">
+                <Box className={cl.markdownContainer}>
+                    <ReactMarkdown remarkPlugins={[remarkToc, remarkGfm]}>{content}</ReactMarkdown>
+                </Box>
+            </Box>
+            <Spacer />
+            <Box className={cl.cardContainer}>
+                <PostFooter
+                    postId={postId}
+                    likes={likes}
+                    comments={comments}
+                    views={views}
+                    onLike={onLike}
+                    onUnlike={onUnlike}
+                />
+            </Box>
+        </Flex>
+    )
+}
---
+++ src/views/containers/PostContaner/PostContainer.jsx
@@ -0,0 +1,33 @@
+import React, { useRef, useCallback } from 'react'
+import { useParams } from 'react-router-dom';
+import { useQueryClient, useInfiniteQuery, useMutation, useQuery } from '@tanstack/react-query';
+import { VStack, Box, Spinner, useToast } from '@chakra-ui/react';
+
+import { PostAPI } from '../../../modules/posts';
+import { Post } from '../../components/Post';
+
+
+export const PostContainer = () => {
+    const { postId } = useParams();
+    const { isLoading ,error, data } = useQuery(['posts', postId], () => PostAPI.getPost(postId))
+
+    if (isLoading) {
+        return (
+            <Spinner
+                thickness='4px'
+                speed='0.25s'
+                emptyColor='gray.200'
+                color='blue.500'
+                size='xl'
+            />
+            )
+        }
+
+    if (error) {
+        return <span>Error: {error}</span>
+    }
+
+    return (
+        <Post {...data}/>
+    )
+}
