dccf169645ca07727ead6f816ffe49ff4423cb0c
Nikolay Grinko <ngrink.io@gmail.com>
2022-11-13 16:03:21 +0300

    Rename "HomePage" to "PopularPostsScreen"
    - update routes
    - add redirect from "/" to "/popular"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Suspense } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { Spinner } from '@chakra-ui/react';
 
 import './assets/css/App.css';
 
 const MainLayout = React.lazy(() => import('./views/layouts/MainLayout'));
-const HomeScreen = React.lazy(() => import('./views/screens/HomeScreen'));
+const PopularPostsScreen = React.lazy(() => import('./views/screens/PopularPostsScreen'));
 const PostScreen = React.lazy(() => import('./views/screens/PostScreen'));
 const PostEditScreen = React.lazy(() => import('./views/screens/PostEditScreen'));
 const PostCreateScreen = React.lazy(() => import('./views/screens/PostCreateScreen'));
@@ -21,13 +21,13 @@ export const App = () => {
                     <Route path="/register" element={<h1>RegisterPage</h1>} />
                 </Route>
                 <Route element={<MainLayout />}>
-                    <Route path="/" element={<HomeScreen />} />
-                    <Route path="/authors" element={<h1>AuthorsPage</h1>} />
-                    <Route path="/authors/:authorId" element={<h1>AuthorPage</h1>} />
-                    <Route path="/posts/popular" element={<h1>PopularPostsPage</h1>} />
+                    <Route path="/" element={<Navigate to="/popular" replace={true} />} />
+                    <Route path="/popular" element={<PopularPostsScreen />} />
                     <Route path="/posts/new" element={<PostCreateScreen />} />
                     <Route path="/posts/:postId" element={<PostScreen />} />
                     <Route path="/posts/:postId/edit" element={<PostEditScreen />} />
+                    <Route path="/authors" element={<h1>AuthorsPage</h1>} />
+                    <Route path="/authors/:authorId" element={<h1>AuthorPage</h1>} />
                     <Route path="*" element={<h1>404 Not found</h1>} />
                 </Route>
             </Routes>
--- src/views/screens/PopularPostsScreen/PopularPostsScreen.jsx
+++ src/views/screens/PopularPostsScreen/PopularPostsScreen.jsx
@@ -4,7 +4,7 @@ import { Box } from '@chakra-ui/react';
 import { PostCardListContainer } from '../../containers/PostCardListContainer';
 
 
-export const HomeScreen = () => {
+export const PopularPostsScreen = () => {
     return (
         <Box maxW="640px" mx="auto" pt="50px" pb="50px">
             <PostCardListContainer />
