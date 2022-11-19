595abd64971a3de9ea6526725f3996071271a1cb
Nikolay Grinko <ngrink.io@gmail.com>
2022-11-19 03:04:20 +0300

    Add fallback image for post preview

--- src/views/components/PostPreview/PostPreview.jsx
+++ src/views/components/PostPreview/PostPreview.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Box, AspectRatio, Image } from '@chakra-ui/react';
 
+import fallback from "../../../assets/img/img-placeholder.jpeg";
+
 
 export const PostPreview = ({ preview }) => {
     return (
         <Box w="100%">
             <AspectRatio ratio={16 / 9}>
-                <Image src={preview} objectFit="cover"/>
+                <Image src={preview} onError={(e)=>{ e.target.onError = null; e.target.src = fallback }} objectFit="cover" />
             </AspectRatio>
         </Box>
     )
